c97caea60a38f3017f7b6905845811a5c11abcbc
Andrew <andreimidari@gmail.com>
2025-04-06 10:55:10 +0200

    final edits

--- blockchain.py
+++ blockchain.py
@@ -2,7 +2,7 @@ import hashlib
 import json
 from time import time
 from uuid import uuid4
-from textwrap import dedent
+# Removed unused import
 from flask import Flask, jsonify, request
 from urllib.parse import urlparse 
 import requests
@@ -197,3 +197,39 @@ def full_chain():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values = request.get_json()
+
+    nodes = values.get('nodes')
+    if nodes is None:
+        return "Error: Please supply a valid list of nodes", 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        'message': 'New nodes have been added',
+        'total_nodes': list(blockchain.nodes),
+    }
+    return jsonify(response), 201
+
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+        response = {
+            'message': 'Our chain was replaced',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Our chain is authoritative',
+            'chain': blockchain.chain
+        }
+
+    return jsonify(response), 200
+
